19c43dc1b69f712300480bf292c18dd5ffc1a805
Fatima-Ezzahra zahid <57708186+Fatima-Ezzahra-Zahid@users.noreply.github.com>
2020-04-05 01:47:52 +0100

    fichiers init de projet Analogue

---
+++ Projet Montre-Analogue/script.js
@@ -0,0 +1,24 @@
+// Séléctionner les aiguilles de montre
+const AIGUILLEHR = document.querySelector("#hour");
+const AIGUILLEMIN = document.querySelector("#minute");
+const AIGUILLESEC = document.querySelector("#second");
+
+//Extraire l'heure actuel à l'aide de l'objet Date()
+
+
+//Ajouter l'heure , minite , seconde  dans des varaiables
+
+
+
+// Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
+// Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
+
+
+// Déplacer les aiguilles 
+function demarrerLaMontre() {
+
+
+
+}
+// Exercuter la fonction chaque second
+var interval = setInterval(demarrerLaMontre, 1000);
