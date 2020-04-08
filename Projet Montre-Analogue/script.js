a23232afa6e7c66a092d3cf58160a26a653c9d08
mehdi <mehdiqarnouf08@gmail.com>
2020-04-08 14:34:44 +0100

    version 1 de la solution projet Montre

--- Projet Montre-Analogue/script.js
+++ Projet Montre-Analogue/script.js
@@ -1,27 +1,30 @@
-// Séléctionner les aiguilles de montre
 const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
-//Extraire l'heure actuel à l'aide de l'objet Date()
 
-let date = new Date();
-
-//Ajouter l'heure , minite , seconde  dans des varaiables
-let heur = date.getHours(); 
-let minite = date.getMinutes();
-let seconde = date.getSeconds();
-
-
-// Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-// Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-
-
-// Déplacer les aiguilles 
 function demarrerLaMontre() {
 
+  //Extraire l'heure actuel à l'aide de l'objet Date()
+    let date = new Date();
+
+    //Ajouter l'heure , minite , seconde  dans des varaiables
+    let seconde = (date.getSeconds())/60;
+    let munite = (date.getMinutes()+seconde)/60;
+    let heur = (date.getHours()+munite)/12;
+    
+    // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde 
+    let degres=seconde*360;
+    let degrem=munite*360;
+    let degreh=heur*360;
+
+    AIGUILLESEC.style.transform = `rotate(${degres}deg)`;
+    AIGUILLEMIN.style.transform = `rotate(${degrem}deg)`;
+    AIGUILLEHR.style.transform = `rotate(${degreh}deg)`;
+
+   
 
 
 }
 // Exercuter la fonction chaque second
-let interval = setInterval(demarrerLaMontre, 1000);
+var interval = setInterval(demarrerLaMontre, 1000);
